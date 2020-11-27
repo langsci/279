f28a32093210c11953fa0599fecffb5d53d6ef41
mstelling <marten.stelling@uni-goettingen.de>
2020-11-27 16:50:43 +0100

    added first files

---
+++ chapters/auto/01.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "01"
+ (lambda ()
+   (LaTeX-add-labels
+    "fig-structure1"
+    "sec-connections"
+    "tab-ms-model"
+    "tab-muysken_OS"
+    "tab-muysken_CS"
+    "tab-muysken"
+    "framework"
+    "fig-structure2"))
+ :latex)
+
